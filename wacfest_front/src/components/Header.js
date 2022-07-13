f5f59f5d188a4da256e616bcfd786cd6ac1b6fcb
SamuelNeveu <samuel.neveu@epitech.eu>
2022-07-13 14:12:36 +0200

    header

--- wacfest_front/src/components/Header.js
+++ wacfest_front/src/components/Header.js
@@ -1,8 +1,21 @@
 import '../stylesheets/header.css';
+import LogoBlack from '../assets/logo_black.png';
 
 export default function Header() {
     return <div className="Header">
-        <div>Logo</div>
-        <div>Billetterie</div>
-    </div>
+        <div className="container-header">
+            <div id="NavLogo">
+                <img src={LogoBlack} alt='Logo' id="logo-black"/>
+            </div>
+
+            <div className="NavBar">
+                <div className="NavItem"><a href=""><h2>Billetterie</h2></a></div>
+                <div className="NavItem"><a href=""><h2>Boutique</h2></a></div>
+                <div className="NavItem"><a href=""><h2>Programme</h2></a></div>
+            </div>
+
+            <div>Panier</div>
+
+            </div>
+        </div>
 }
