01aabf239af8743b727ebfec43b7bf6df9882939
nalor <orlane.margueritte@epitech.eu>
2022-07-18 15:51:27 +0200

    card billet premium

--- wacfest_front/src/components/Home.js
+++ wacfest_front/src/components/Home.js
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import StandardCard from "./StandardCard";
+import PremiumCard from "./PremiumCard";
 
 export default function Home() {
     return <div>
         <Header/>
         {/*<Footer/>*/}
         <StandardCard/>
+        <PremiumCard/>
     </div>;
 }
