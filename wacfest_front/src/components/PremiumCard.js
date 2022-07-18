9cffc5b9f0a6af3bd1aaeec07cf2c759b2b500aa
nalor <orlane.margueritte@epitech.eu>
2022-07-18 20:50:35 +0200

    style card

--- wacfest_front/src/components/PremiumCard.js
+++ wacfest_front/src/components/PremiumCard.js
@@ -1,19 +1,32 @@
+import '../stylesheets/Card.css'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Premium from '../assets/Premium.png'
+import Standard from "../assets/Standard.png";
 
 export default function PremiumCard(){
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={Premium} />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
-    )
+        <>
+            {[
+                'Light',
+            ].map((variant) => (
+                <Card id="card" className="text-center"  bg={variant.toLowerCase()}
+                      key={variant}
+                      text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+                      style={{ width: '22rem' }}
+                        border='light'>
+                    <Card.Img variant="top" src={Premium} style={{ width: '22rem' }}/>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="dark">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            ))}
+        </>
+    );
+
 }
--- wacfest_front/src/components/StandardCard.js
+++ wacfest_front/src/components/StandardCard.js
@@ -1,20 +1,30 @@
-
+import '../stylesheets/Card.css'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Standard from '../assets/Standard.png'
+import Standard from '../assets/Standard.png';
 
    export default function StandardCard() {
         return (
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={Standard} />
+            <>
+                {[
+                    'Light',
+                ].map((variant) => (
+            <Card id="card" className="text-center"  bg={variant.toLowerCase()}
+                  key={variant}
+                  text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+                  style={{ width: '22rem' }}
+                  border="light">
+                <Card.Img variant="top" src={Standard} style={{ width: '22rem' }} />
                 <Card.Body>
                     <Card.Title>Card Title</Card.Title>
                     <Card.Text>
                         Some quick example text to build on the card title and make up the
                         bulk of the card's content.
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <Button variant="dark">Go somewhere</Button>
                 </Card.Body>
             </Card>
-        )
+                ))}
+            </>
+        );
     }
