3c25a047cfb3d569800d64514c5ba9b53359a1df
nalor <orlane.margueritte@epitech.eu>
2022-07-18 15:00:09 +0200

    card-ticket/button navabr

--- wacfest_front/src/components/Header.js
+++ wacfest_front/src/components/Header.js
@@ -1,8 +1,10 @@
 import '../stylesheets/header.css';
 
+
 export default function Header() {
     return <div className="Header">
         <div>Logo</div>
         <div>Billetterie</div>
+        <Button variant="primary">Primary</Button>{' '}
     </div>
 }
--- wacfest_front/src/components/Home.js
+++ wacfest_front/src/components/Home.js
@@ -1,8 +1,10 @@
 import Header from "./Header";
+import Tickets from "./Tickets";
 
 export default function Home() {
     return <div>
         <Header/>
         {/*<Footer/>*/}
+        <Tickets/>
     </div>;
 }
