beb8dca35fff887009a60e08e279e85e79f12875
nalor <orlane.margueritte@epitech.eu>
2022-07-20 15:48:34 +0200

    Card products

--- wacfest_front/src/components/Home.js
+++ wacfest_front/src/components/Home.js
@@ -2,7 +2,9 @@ import Header from "./Header";
 import HomeCard from "./HomeCard";
 import Premium from '../assets/Premium.png'
 import Standard from "../assets/Standard.png";
+import Prod from  "../assets/polo 1.png"
 import {Container, Row} from "react-bootstrap";
+import ProductsCard from "./Products";
 
 export default function Home() {
     return <div>
@@ -15,6 +17,11 @@ export default function Home() {
                 <HomeCard image={Standard} />
             </Row>
 
+        </Container>
+
+        <Container>
+                <ProductsCard image={Prod}/>
+            <ProductsCard image={Prod}/>
         </Container>
 
     </div>;
---
+++ wacfest_front/src/components/Products.js
@@ -0,0 +1,32 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import {Col, Row} from "react-bootstrap";
+
+export default function ProductsCard({image}){
+    return(
+
+            <div className="d-flex justify-content-center">
+                <Row xs={1} md={3} className="g-4">
+                    {Array.from({ length: 3 }).map((_, idx) => (
+                        <Col>
+                            <Card style={{ width: '22rem' }}>
+                                <Card.Img variant="top" src={image} style={{ width: '20rem' }} />
+                                <Card.Body className="text-center">
+                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Text >
+                                        This is a longer card with supporting text below as a natural
+                                        lead-in to additional content. This content is a little bit
+                                        longer.
+                                    </Card.Text>
+                                    <Button variant="dark">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+
+            </div>
+
+
+    );
+}
