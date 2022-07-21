e32440c05568d6e1dc0db4742d4b019a80300ba9
nalor <orlane.margueritte@epitech.eu>
2022-07-21 10:02:00 +0200

    padding button

--- wacfest_front/src/components/ProdButton.js
+++ wacfest_front/src/components/ProdButton.js
@@ -3,14 +3,17 @@ import Button from 'react-bootstrap/Button';
 
 export default function Redirect(){
     return(
-        <Button
-            className="d-flex justify-content-left"
-            variant='dark'
-            onClick={() => {
-                alert('clicked');
-            }}
-        >
-            Voir la boutique
-        </Button>
+        <div className="p-5" style="width: 200px;" style={{display:'flex',justifyContent:'flex-end'}}>
+            <Button
+
+                style={{width: 200, }}
+                variant='dark'
+                onClick={() => {
+                    alert('clicked');
+                }}
+            >
+                Voir la boutique
+            </Button>
+        </div>
     );
 }
