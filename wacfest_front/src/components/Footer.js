b8053be26163eaa33a154807908c71c71617dd0b
Kévin <kevin.jourdain@epitech.eu>
2022-07-21 11:51:47 +0200

    Style Footer

--- wacfest_front/src/components/Footer.js
+++ wacfest_front/src/components/Footer.js
@@ -1,6 +1,8 @@
 import '../stylesheets/Footer.css';
 
 import logo from '../assets/logo_black.png';
+import facebook from '../assets/facebook.png';
+import instagram from '../assets/instagram.png';
 
 export default function Header(){
     return <div className="footer-container">
@@ -12,11 +14,12 @@ export default function Header(){
         <div className="footer">
             <div class="container_footer">
                 <img src={logo}
-                     width="82"
-                     height="82"
+                     width="130"
+                     height="130"
                 />
                 <div class="bloc">
                     <h3>NAVIGATION</h3>
+                    <div className="form-blue"></div>
                     <ul class="list_bloc_footer">
                         <li><a class="list_footer" href="#">BILLETTERIE</a></li>
                         <li><a class="list_footer" href="#">BOUTIQUE</a></li>
@@ -25,6 +28,7 @@ export default function Header(){
                 </div>
                 <div class="bloc">
                     <h3>SOCIÉTÉ</h3>
+                    <div className="form-yellow"></div>
                     <ul class="list_bloc_footer">
                         <li><a class="list_footer" href="#">CGU</a></li>
                         <li><a className="list_footer" href="#">CGV</a></li>
@@ -33,9 +37,10 @@ export default function Header(){
                 </div>
                 <div class="bloc">
                     <h3>RÉSEAUX SOCIAUX</h3>
+                    <div className="form-red"></div>
                     <ul class="list_bloc_footer">
-                        <li><a class="list_footer" href="#">logo fb</a></li>
-                        <li><a class="list_footer" href="#">logo insta</a></li>
+                        <li><a className="list_footer" href="#"><img src={facebook} width="52" height="52"/></a></li>
+                        <li><a className="list_footer" href="#"><img src={instagram} width="52" height="52"/></a></li>
                         </ul>
                     </div>
                 </div>
