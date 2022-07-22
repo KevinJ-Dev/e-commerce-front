ca29936ebdabde6de7527ad970350f85e3c9dc29
Kévin <kevin.jourdain@epitech.eu>
2022-07-22 15:20:07 +0200

    merge routing

--- wacfest_front/src/App.js
+++ wacfest_front/src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import Home from "./components/Home";
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Ticketing from "./components/Ticketing";
 
 function App() {
   return (
           <Router>
               <Header/>
+
               <Routes>
                   <Route exact path="/" element={<Home/>}/>
-                  {/*<Route path="/about" component={About} />*/}
-                  {/*<Route path="/topics" component={Topics} />*/}
+                  <Route path="/ticket" element={<Ticketing/>}/>
               </Routes>
+
+              <Footer/>
           </Router>
   );
 }
--- wacfest_front/src/components/Header.js
+++ wacfest_front/src/components/Header.js
@@ -4,18 +4,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useNavigate } from "react-router-dom";
 
 import logo from '../assets/logo_black.png';
 
 export default function Header() {
+    let navigate = useNavigate();
+
     return <div className="Header">
         <Navbar bg="light" variant="light">
             <Container>
                 {/*<Navbar.Brand href="#home">Navbar</Navbar.Brand>*/}
                 <img src={logo}
                      width="82"
-                     height="82"
-                />
+                     height="82" onClick={() => {
+                            navigate('/');
+                     }}/>
+
                 <Nav className="me-auto">
                     <Nav.Link href="/ticket">Billetterie</Nav.Link>
                     <Nav.Link href="/shop">Boutique</Nav.Link>
