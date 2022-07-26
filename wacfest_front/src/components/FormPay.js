f666b640fe92a56ebf382d0ffaf1fcc441652317
nalor <orlane.margueritte@epitech.eu>
2022-07-26 14:53:55 +0200

    alert succes

--- wacfest_front/src/components/FormPay.js
+++ wacfest_front/src/components/FormPay.js
@@ -19,14 +19,18 @@ export default function FormValidate(){
         <FormText className='p-3'>Veuillez saisir vos informations personnelles:</FormText>
         <form onSubmit={handleSubmit(onSubmit)}>
             {/* register your input into the hook by invoking the "register" function */}
-        <input className="p-3 m-3"  {...register("lastname")} placeholder="Nom"/>
+        <input className="p-3 m-3"  {...register("lastname",{ required: true })} placeholder="Nom"/>
             {/* include validation with required or other standard HTML validation rules */}
+            {/* errors will return when field validation fails  */}
+            {errors.lastname && <span>Ce champ doit ếtre rempli </span>}
         <input  className="p-3 m-3"{...register("name", { required: true })} placeholder="Prénom" />
             {/* errors will return when field validation fails  */}
             {errors.name && <span>Ce champ doit ếtre rempli </span>}
-        <input className="p-3 m-3" {...register('email')} type="email" placeholder="Email" />
+        <input className="p-3 m-3" {...register('email',{ required: true })} type="email" placeholder="Email" />
+            {/* errors will return when field validation fails  */}
+            {errors.email && <span>Ce champ doit ếtre rempli </span>}
 
-            <Button type="submit" variant='dark' onClick={() => alert('Vous avez reçu un mail pour valider la transaction')}>Envoyer</Button>
+            <Button type="submit" variant='dark' onClick={() => alert('Succés')}>Envoyer</Button>
         </form>
     </FormGroup>
     );
